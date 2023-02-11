84fc13448af1ffa9fee61c36f75f95314a6b3aa6
daze573 <124431662+daze573@users.noreply.github.com>
2023-02-11 09:49:58 +0000

    [Add] 2-6 休憩

---
+++ lesson6-1.rb
@@ -0,0 +1,9 @@
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+input_key1 = gets
+puts "#{input_key1}"
+input_key2 = gets
+puts "#{input_key2}"
+puts "計算結果を出力します"
+puts "#{input_key1} *.to_i #{input_key2}"
+puts "計算を終了します"
